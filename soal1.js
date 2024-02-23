03bd53837f89de85cdedf9c67f019a8c3cf9a933
Taufik Hidayat <hidytt.taufik@gmail.com>
2024-02-23 21:39:52 +0700

    update reusable code

--- soal1.js
+++ soal1.js
@@ -3,7 +3,7 @@ const rumus = require('./node_modules/rumus');
 // hitung luas persegi
 let sisiPersegi1 = 10;
 let sisiPersegi2 = 20;
-console.log("Luas Persegi: " + rumus.luasPersegi(sisiPersegi1, sisiPersegi2));
+console.log("Luas Persegi: " + rumus.hitungLuas(sisiPersegi1, sisiPersegi2));
 
 // hitung keliling persegi
 let sisiPersegi = 30;
@@ -12,7 +12,7 @@ console.log("Keliling Persegi: " + rumus.kelilingPersegi(sisiPersegi));
 // hitung luas persegi panjang
 let panjang = 40;
 let lebar = 50;
-console.log("Luas Persegi Panjang: " + rumus.luasPersegiPanjang(panjang, lebar));
+console.log("Luas Persegi Panjang: " + rumus.hitungLuas(panjang, lebar));
 
 // hitung keliling persegi panjang
 console.log("Keliling Persegi Panjang: " + rumus.kelilingPersegiPanjang(panjang, lebar));
